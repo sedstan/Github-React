92ea2023dea6832c87e3c0f0e245afd0c775e8ca
sedstan <sedky.stanley.said@gmail.com>
2018-04-11 09:10:52 +0100

    Updated ReposCard --Mapped object and rendered repos --TODO: solve error in the console

--- src/components/reposCard/reposCard.js
+++ src/components/reposCard/reposCard.js
@@ -3,12 +3,28 @@ import './reposCard.css';
 // import InnerCard from "../innerCard/innerCard";
 
 class ReposCard extends Component {
+  displayRepos = () => {
+    return this.props.data.current && this.props.data.current.repositories.edges.map((item) => {
+      {item.node}
+    })
+  }
   render() {   
-    console.log(this.props.data.current && this.props.data.current.repositories.edges[0].node) 
+    // console.log(this.props.data.current && this.props.data.current.repositories.edges) 
     return (
       <section>
         <div>
-          {this.props.data.current && this.props.data.current.repositories.edges[0].node}
+          {/* TODO: map this data */}
+          {this.props.data.current && this.props.data.current.repositories.edges.map((item) => {
+            return (
+              <div>
+                <a href={item.node.url}>
+                  <h4>{item.node.name}</h4>
+                  <p>{item.node.description}</p>
+                </a>
+              </div>
+            )
+           
+          })}
         </div>
       </section>
     )
