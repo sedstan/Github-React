374f59ffdce5b29dad084e827a601c3069124732
sedstan <sedky.stanley.said@gmail.com>
2018-04-12 06:41:02 +0100

    ReposCard --Created key for items in object --Rendered with no errors

--- src/components/reposCard/reposCard.js
+++ src/components/reposCard/reposCard.js
@@ -1,31 +1,21 @@
 import React, { Component } from "react";
 import './reposCard.css';
-// import InnerCard from "../innerCard/innerCard";
+import InnerCard from "../innerCard/innerCard";
 
 class ReposCard extends Component {
-  displayRepos = () => {
-    return this.props.data.current && this.props.data.current.repositories.edges.map((item) => {
-      {item.node}
-    })
-  }
-  render() {   
-    // console.log(this.props.data.current && this.props.data.current.repositories.edges) 
+  render() {
     return (
-      <section>
-        <div>
-          {/* TODO: map this data */}
-          {this.props.data.current && this.props.data.current.repositories.edges.map((item) => {
-            return (
-              <div>
-                <a href={item.node.url}>
-                  <h4>{item.node.name}</h4>
-                  <p>{item.node.description}</p>
-                </a>
-              </div>
-            )
-           
-          })}
-        </div>
+      <section className="repos">
+        {this.props.data.current && this.props.data.current.repositories.edges.map((item, index) => {
+          return (
+            <div className="repos-card" key={index}>
+              <a href={item.node.url}>
+                <h4>{item.node.name}</h4>
+                <p>{item.node.description}</p>
+              </a>
+            </div>
+          )
+        })}
       </section>
     )
   }
