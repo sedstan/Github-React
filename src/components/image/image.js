94a5f20eb847a60204fe93368a8a6105e796bc72
sedstan <sedky.stanley.said@gmail.com>
2018-04-14 09:35:34 +0100

    Styles --Added normalize.css --styled cards

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -3,11 +3,13 @@ import "./image.css";
 
 
 const Image = () => {
-	return(
-			<img src="https://avatars0.githubusercontent.com/u/13971876?v=4"
-				 alt="me"
-			 	 className="image"/>
-		)
+  return (
+    <img src="https://avatars0.githubusercontent.com/u/13971876?v=4"
+      alt="me"
+      className="image"
+      heigt={300}
+      width={300} />
+  )
 }
 
 export default Image;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import AppContainer from "./containers/AppContainer";
 import store from "./store";
 import './index.css';
+import  'normalize.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Provider store={store}><AppContainer /></Provider>, document.getElementById('root'));
