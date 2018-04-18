c8d01eeb15ea5bbdb7e121a6626330edfb4c7869
sedstan <sedky.stanley.said@gmail.com>
2018-04-18 19:15:04 +0100

    Updated actions --Added languages to query --Updated reposCard component

--- src/actions/site.js
+++ src/actions/site.js
@@ -2,15 +2,15 @@ import axios from "axios";
 import token from "../config/config.js";
 
 export function getProfile() {
-	return function(dispatch) {
-		axios({
+  return function (dispatch) {
+    axios({
       method: 'post',
       url: 'https://api.github.com/graphql',
       headers: {
         'Authorization': 'Bearer ' + token
       },
-			data: {
-				query: `
+      data: {
+        query: `
 					query LogIn  { 
 						 viewer {
 							login
@@ -25,35 +25,35 @@ export function getProfile() {
   					}
 					}
 			`
-			}
-		})
-		.then(response => {
-			// console.log(response.data);
-			dispatch({
-				type: "FETCH_USER_PROFILE_FULLFILLED",
-				payload: response.data
-			});
-		})
-		.catch(error => {
-			console.log(error);
-			dispatch({
-				type: "FETCH_USER_PROFILE_FAILED",
-				payload: error
-			});
-		});
-	}
+      }
+    })
+      .then(response => {
+        // console.log(response.data);
+        dispatch({
+          type: "FETCH_USER_PROFILE_FULLFILLED",
+          payload: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch({
+          type: "FETCH_USER_PROFILE_FAILED",
+          payload: error
+        });
+      });
+  }
 }
 
 export function getRepos() {
-	return function(dispatch) {
-		axios({
+  return function (dispatch) {
+    axios({
       method: 'post',
       url: 'https://api.github.com/graphql',
       headers: {
         'Authorization': 'Bearer ' + token
       },
-			data: {
-				query: `
+      data: {
+        query: `
 					query Repos  { 
 						 viewer {
 								repositories(last: 10) {
@@ -61,28 +61,35 @@ export function getRepos() {
 									node {
 										name
 										description
-										url
+                    url
+                    languages(last: 10) {
+                      edges {
+                        node {
+                          name
+                        }
+                      }
+                    }
 									}
 								}
 							}
   					}
 					}
 			`
-			}
-		})
-		.then(response => {
-			// console.log(response.data);
-			dispatch({
-				type: "FETCH_USER_REPOS_FULLFILLED",
-				payload: response.data
-			});
-		})
-		.catch(error => {
-			console.log(error);
-			dispatch({
-				type: "FETCH_USER_REPOS_FAILED",
-				payload: error
-			});
-		});
-	}
+      }
+    })
+      .then(response => {
+        // console.log(response.data);
+        dispatch({
+          type: "FETCH_USER_REPOS_FULLFILLED",
+          payload: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch({
+          type: "FETCH_USER_REPOS_FAILED",
+          payload: error
+        });
+      });
+  }
 }
--- src/components/reposCard/reposCard.js
+++ src/components/reposCard/reposCard.js
@@ -2,16 +2,24 @@ import React, { Component } from "react";
 import './reposCard.css';
 
 class ReposCard extends Component {
-  render() {
+  render() {    
     return (
       <section className="repos">
         {this.props.data.current && this.props.data.current.repositories.edges.map((item, index) => {
+          console.log(this.props.data.current.repositories.edges)
           return (
             <div className="repos-card" key={index}>
-              <div className="repost-card-inner">
+              <div className="repos-card--inner">
                 <a href={item.node.url}>
                   <h4>{item.node.name}</h4>
                   <p>{item.node.description}</p>
+                  {item.node.languages.edges.map((lang, i) => {
+                    return (
+                      <div className="repos-card--inner--lang" key={i}>
+                        <p>{lang.node.name}</p>
+                      </div>
+                    )
+                  })}
                 </a>
               </div>
             </div>
