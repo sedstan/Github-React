8ae35e37e82318b03fc8af438e7d6fe983de61dd
sedstan <sedky.stanley.said@gmail.com>
2018-04-23 07:27:19 +0100

    Profile card --Removed InnerCard component --Moved icons inline with text --Added div for overlay

--- src/components/profileCard/profileCard.js
+++ src/components/profileCard/profileCard.js
@@ -9,19 +9,21 @@ import { GoMention } from 'react-icons/lib/go'
 
 const ProfileCard = (props) => {
   console.log(props.data.current);
-  
+
   return (
     <section className="card">
-      <div>
-        <Image />
+      <div className="card--overlay">
+        <div>
+          <Image />
+        </div>
       </div>
       <div className="card--profile">
-         {props.data.current && <h2>{props.data.current.name}</h2>}
-        <GoMention />{props.data.current && <p> {props.data.current.login}</p>}
-         {props.data.current && <p>{props.data.current.bio}</p>}
-        <GoLocation />{props.data.current && <p>{props.data.current.location}</p>}
-        <GoOrganization />{props.data.current && <p>{props.data.current.company}</p>}
-        <GoMail />{props.data.current && <p>{props.data.current.email}</p>}
+        {props.data.current && <h2>{props.data.current.name}</h2>}
+        {props.data.current && <p><GoMention className="card--icons" /> {props.data.current.login}</p>}
+        {props.data.current && <p>{props.data.current.bio}</p>}
+        {props.data.current && <p><GoLocation className="card--icons" />{props.data.current.location}</p>}
+        {props.data.current && <p><GoOrganization className="card--icons" />{props.data.current.company}</p>}
+        {props.data.current && <p><GoMail className="card--icons" />{props.data.current.email}</p>}
       </div>
     </section>
   )
