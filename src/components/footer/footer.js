c4381503306457f8421dfd0765929bea29946344
Sedky S Said <sedstan@Sedkys-MacBook-Pro.local>
2018-05-11 23:44:20 +0200

    Adding copyright to footer.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,12 +3,16 @@ import { Date } from "core-js";
 
 export default class Footer extends Component {
   displayCopyright = () => {
-    // document.write(new Date().getFullYear());
+    // document.getElementsByClassName('footer');
+    // new Date().getFullYear()
+    return (
+      <p>Copyright &copy;   </p>
+    )
   } 
   render() {
     console.log(this.props);
     return(
-      <footer>
+      <footer className="footer">
         {this.displayCopyright()}
       </footer>
     )
