a4ede744bb886bb40361f870a9ee23958338d268
SedStan <me@sedkystanleysaid.com>
2019-05-27 02:47:27 +0100

    Refactors react-router-dom browser history module.

--- src/store.js
+++ src/store.js
@@ -1,10 +1,10 @@
 import { applyMiddleware, createStore, compose } from "redux";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
-import promise from "redux-promise-middleware";
+import {createPromise} from "redux-promise-middleware";
 import reducer from "./reducers";
 import "./index.css";
 
-const middleware = applyMiddleware(promise(), thunk, createLogger());
+const middleware = applyMiddleware(createPromise(), thunk, createLogger());
 
 export default createStore(reducer, middleware, compose, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
