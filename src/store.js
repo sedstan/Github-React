dd8aa28814638955c5ca2c1fee467bd9a5ca736f
SedStan <me@sedkystanleysaid.com>
2019-06-03 23:59:51 +0100

    Fixes React Errors --Updates all packages --Fixes Redux store errors --Fixes favicon error and adds additional meta and links for icons

--- src/store.js
+++ src/store.js
@@ -2,9 +2,10 @@ import { applyMiddleware, createStore, compose } from "redux";
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 import {createPromise} from "redux-promise-middleware";
-import reducer from "./reducers";
+import reducers from "./reducers";
 import "./index.css";
 
-const middleware = applyMiddleware(createPromise(), thunk, createLogger());
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancer(applyMiddleware(createPromise(), thunk, createLogger())));
 
-export default createStore(reducer, middleware, compose, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export default store;
