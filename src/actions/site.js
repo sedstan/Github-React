13cea1230d50af4fb6980106b909f626fa5a4f6d
SedStan <me@sedkystanleysaid.com>
2019-06-04 11:17:57 +0100

    Updates formatting.

--- src/actions/site.js
+++ src/actions/site.js
@@ -2,56 +2,56 @@ import axios from "axios";
 import token from "../config/config.js";
 
 export function getProfile() {
-  return function (dispatch) {
-    axios({
-        method: 'post',
-        url: 'https://api.github.com/graphql',
-        headers: {
-          'Authorization': 'Bearer ' + token
-        },
-        data: {
-          query: `
+	return function (dispatch) {
+		axios({
+			method: 'post',
+			url: 'https://api.github.com/graphql',
+			headers: {
+				'Authorization': 'Bearer ' + token
+			},
+			data: {
+				query: `
 					query LogIn  { 
 						 viewer {
-               avatarUrl
-              name
-              login
-              bio
-              location
-              company
-              email
-  					}
+			                avatarUrl
+			                name
+			                login
+			                bio
+			                location
+			                company
+			                email
+			             }
 					}
 			`
-        }
-      })
-      .then(response => {
-        console.log(response.data);
-        dispatch({
-          type: "FETCH_USER_PROFILE_FULLFILLED",
-          payload: response.data
-        });
-      })
-      .catch(error => {
-        console.log(error);
-        dispatch({
-          type: "FETCH_USER_PROFILE_FAILED",
-          payload: error
-        });
-      });
-  }
+			}
+		})
+			.then(response => {
+				console.log(response.data);
+				dispatch({
+					type: "FETCH_USER_PROFILE_FULLFILLED",
+					payload: response.data
+				});
+			})
+			.catch(error => {
+				console.log(error);
+				dispatch({
+					type: "FETCH_USER_PROFILE_FAILED",
+					payload: error
+				});
+			});
+	}
 }
 
 export function getRepos() {
-  return function (dispatch) {
-    axios({
-        method: 'post',
-        url: 'https://api.github.com/graphql',
-        headers: {
-          'Authorization': 'Bearer ' + token
-        },
-        data: {
-          query: `
+	return function (dispatch) {
+		axios({
+			method: 'post',
+			url: 'https://api.github.com/graphql',
+			headers: {
+				'Authorization': 'Bearer ' + token
+			},
+			data: {
+				query: `
 					query Repos  { 
 						 viewer {
 								repositories(last: 10) {
@@ -59,36 +59,36 @@ export function getRepos() {
 									node {
 										name
 										description
-                    url
-                    languages(last: 10) {
-                      edges {
-                        node {
-                          name
-                          color
-                        }
-                      }
-                    }
+                                        url
+                                        languages(last: 10) {
+					                      edges {
+					                        node {
+					                          name
+					                          color
+					                        }
+					                      }
+                                        }
 									}
 								}
 							}
-  					}
+  					    }
 					}
 			`
-        }
-      })
-      .then(response => {
-        // console.log(response.data);
-        dispatch({
-          type: "FETCH_USER_REPOS_FULLFILLED",
-          payload: response.data
-        });
-      })
-      .catch(error => {
-        console.log(error);
-        dispatch({
-          type: "FETCH_USER_REPOS_FAILED",
-          payload: error
-        });
-      });
-  }
+			}
+		})
+			.then(response => {
+				// console.log(response.data);
+				dispatch({
+					type: "FETCH_USER_REPOS_FULLFILLED",
+					payload: response.data
+				});
+			})
+			.catch(error => {
+				console.log(error);
+				dispatch({
+					type: "FETCH_USER_REPOS_FAILED",
+					payload: error
+				});
+			});
+	}
 }
