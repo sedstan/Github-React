dd4861ad1bc2d871553f3d9583e69c4e700a862e
SedStan <me@sedkystanleysaid.com>
2019-06-11 08:43:33 +0100

    Converts pixels to emphemeral unit.

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,18 +2,18 @@ import React, { Component } from "react";
 import './footer.scss';
 
 export default class Footer extends Component {
-  displayCopyright = () => {
-    let today = new Date();
-    let year = today.getFullYear();
-    return (
-      <p>Copyright &copy; {year}</p>
-    )
-  };
-  render() {
-    return(
-      <footer className="footer">
-         {this.displayCopyright()}
-      </footer>
-    )
-  }
+	displayCopyright = () => {
+		let today = new Date();
+		let year = today.getFullYear();
+		return (
+			<p>Copyright &copy; {year}</p>
+		)
+	};
+	render() {
+		return(
+			<footer className="footer">
+				 {this.displayCopyright()}
+			</footer>
+		)
+	}
 }
--- src/components/reposCard/reposCard.js
+++ src/components/reposCard/reposCard.js
@@ -2,31 +2,31 @@ import React from "react";
 import './reposCard.scss';
 
 const ReposCard = props => {
-  console.log(props);
-  return (
-    <section className="layout">
-      {props.data.current && props.data.current.repositories.edges.map((repo, index) => {
-        return (
-          <div className="card" key={index}>
-            <a href={repo.node.url}>
-              <div className="card--repos">
-                <h4>{repo.node.name}</h4>
-                <p>{repo.node.description}</p>
-                <h3>Languages:</h3>
-                {repo.node.languages.edges.map((lang, i) => {
-                  return (
-                    <div key={i}>
-                      <p style={{color: `${lang.node.color}`}}>{lang.node.name}</p>
-                    </div>
-                  )
-                })}
-              </div>
-            </a>
-          </div>
-        )
-      })}
-    </section>
-  )
+	console.log(props);
+	return (
+		<section className="layout">
+			{props.data.current && props.data.current.repositories.edges.map((repo, index) => {
+				return (
+					<div className="card" key={index}>
+						<a href={repo.node.url}>
+							<div className="card--repos">
+								<h4>{repo.node.name}</h4>
+								<p>{repo.node.description}</p>
+								<h3>Languages:</h3>
+								{repo.node.languages.edges.map((lang, i) => {
+									return (
+										<div key={i}>
+											<p style={{color: `${lang.node.color}`}}>{lang.node.name}</p>
+										</div>
+									)
+								})}
+							</div>
+						</a>
+					</div>
+				)
+			})}
+		</section>
+	)
 };
 
 export default ReposCard
